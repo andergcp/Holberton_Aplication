01f724794d98b25d2125fec0114c353a7f751fcb
andergcp <andergcp@hotmail.com>
2020-05-01 06:18:48 -0500

    [ADDING FILES]

---
+++ behavior.js
@@ -0,0 +1,92 @@
+document.addEventListener("DOMContentLoaded", function(event) 
+{
+    
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() 
+        {
+            if (thumbnailElement.className == "small")
+                {thumbnailElement.className = ""}
+            else 
+                {thumbnailElement.className = "small"}        
+            
+        });
+    
+    var thumbnailElement2 = document.getElementById("smart_thumbnail2");
+    thumbnailElement2.addEventListener("click", function() 
+        {
+            if (thumbnailElement2.className == "small")
+                {thumbnailElement2.className = ""}
+            else 
+                {thumbnailElement2.className = "small"}        
+            
+        });
+    
+    var thumbnailElement3 = document.getElementById("smart_thumbnail3");
+    thumbnailElement3.addEventListener("click", function() 
+        {
+            if (thumbnailElement3.className == "small")
+                {thumbnailElement3.className = ""}
+            else 
+                {thumbnailElement3.className = "small"}        
+            
+        });
+    
+    var thumbnailElement4 = document.getElementById("smart_thumbnail4");
+    thumbnailElement4.addEventListener("click", function() 
+        {
+            if (thumbnailElement4.className == "small")
+                {thumbnailElement4.className = ""}
+            else 
+                {thumbnailElement4.className = "small"}        
+            
+        });
+    
+    var thumbnailElement5 = document.getElementById("smart_thumbnail5");
+    thumbnailElement5.addEventListener("click", function() 
+        {
+            if (thumbnailElement5.className == "small")
+                {thumbnailElement5.className = ""}
+            else 
+                {thumbnailElement5.className = "small"}
+        });
+    
+    var thumbnailElement6 = document.getElementById("smart_thumbnail6");
+    thumbnailElement6.addEventListener("click", function() 
+        {
+            if (thumbnailElement6.className == "small")
+                {thumbnailElement6.className = ""}
+            else 
+                {thumbnailElement6.className = "small"}
+        });
+    
+    var thumbnailElement7 = document.getElementById("smart_thumbnail7");
+    thumbnailElement7.addEventListener("click", function() 
+        {
+            if (thumbnailElement7.className == "small")
+                {thumbnailElement7.className = ""}
+            else 
+                {thumbnailElement7.className = "small"}
+        });
+    
+    var thumbnailElement8 = document.getElementById("smart_thumbnail8");
+    thumbnailElement8.addEventListener("click", function() 
+        {
+            if (thumbnailElement8.className == "small")
+                {thumbnailElement8.className = ""}
+            else 
+                {thumbnailElement8.className = "small"}
+        });
+    
+    var thumbnailElement9 = document.getElementById("smart_thumbnail9");
+    thumbnailElement9.addEventListener("click", function() 
+        {
+            if (thumbnailElement9.className == "small")
+                {thumbnailElement9.className = ""}
+            else 
+                {thumbnailElement9.className = "small"}
+        });
+    
+    
+
+});
+
